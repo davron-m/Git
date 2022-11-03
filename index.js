c4c81a5b87d652e4c4d167054c911c09827ceb86
Davron <104767260+davron-m@users.noreply.github.com>
2022-11-03 22:59:08 +0300

    Updated

--- index.js
+++ index.js
@@ -11,3 +11,67 @@ const firstPost = {
 }
 
 console.log(newPost(firstPost))
+
+// Array vs Object
+
+const  myArray = [1, 3, 77, 50]
+
+const myObject = {
+    0: 1,
+    1: 2,
+    2: 3,
+}
+
+console.log(myObject)
+
+//Array
+// Чтения значения массива
+
+const mArray = [20, 56, 90]
+
+console.log(mArray[2])              //Индекс массива
+
+console.log(mArray.length)          // Длина массива
+
+mArray[1] = true                    //Изменить значения массива
+
+// Метод PUSH добавляеть значения конец массива
+
+mArray.push(false)
+
+//Метод POP удаления последная значения 
+
+mArray.pop()
+
+const reElement = mArray.pop()
+
+console.log(reElement)
+
+//Метод  UNSHIFT добавляеть значения начало массива
+
+ mArray.unshift(22)
+ mArray.unshift(1)
+
+ //Метод  SHIFT удаляеть первая  значения  массива
+
+ mArray.shift()
+ console.log(mArray)
+
+ //Метод FOREACH
+
+ mArray.forEach(el => console.log(el * 3))
+
+//Метод MAP
+
+let newArray = mArray.map(el => el * 2)
+
+newArray = mArray.map((el) =>{
+    return el * 4
+})
+
+
+console.log(newArray)
+
+
+
+
